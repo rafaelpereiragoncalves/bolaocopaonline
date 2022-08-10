24046a3fa039d852195874624ecb265e3e9935bc
Rafael <rafael.goncalves@tinnova.com.br>
2022-08-10 15:59:56 -0300

    adjust

--- src/main/kotlin/com/bolaocopaonline/bolaocopaonline/integration/controller/KeyGroupController.kt
+++ src/main/kotlin/com/bolaocopaonline/bolaocopaonline/integration/controller/KeyGroupController.kt
@@ -6,8 +6,12 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
+@RestController
+@RequestMapping("keygroups")
 data class KeyGroupController(
     private val keyGroupService: KeyGroupService
 ) {
