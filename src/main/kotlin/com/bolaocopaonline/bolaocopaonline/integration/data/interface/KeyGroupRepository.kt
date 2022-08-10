3072cb11674f7d21da1ea97f3af24d1f1c93448e
Rafael <rafael.goncalves@tinnova.com.br>
2022-08-10 15:55:03 -0300

    adjust

--- src/main/kotlin/com/bolaocopaonline/bolaocopaonline/integration/data/interface/KeyGroupRepository.kt
+++ src/main/kotlin/com/bolaocopaonline/bolaocopaonline/integration/data/interface/KeyGroupRepository.kt
@@ -2,10 +2,12 @@ package com.bolaocopaonline.bolaocopaonline.integration.data.`interface`
 
 import com.bolaocopaonline.bolaocopaonline.integration.data.models.KeyGroup
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface KeyGroupRepository : JpaRepository<KeyGroup, Long> {
 
+    @Query("SELECT kg FROM User as kg")
     fun getAllKeyGroups(): List<KeyGroup>
 }
